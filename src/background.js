e7ad2f937676c13842707548e983435bb0f59006
Rishab Bhatt <bhattrishab@gmail.com>
2021-12-14 18:44:13 -0500

    v1.0.8 - Fix Falcon not starting by default

--- src/background.js
+++ src/background.js
@@ -1,24 +1,7 @@
-import FalconStorage from "./services/storage";
-
-var isExtensionOn = true;
-
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-
     if (changeInfo.status === 'complete') {
         if (/^https:\/\/owl\.uwo\.ca/.test(tab.url)) {
             chrome.tabs.executeScript(null, {file: './assets/redirect-handler.js'})
         }
     }
 })
-
-console.log(2323);
-
-// FalconStorage.sync().get('falconEnabled').then(data => {
-//     console.log(222, data);
-// });
-//
-// chrome.runtime.onInstalled.addListener(function(details){
-//     FalconStorage.sync().set({
-//         falconEnabled: true,
-//     });
-// })
